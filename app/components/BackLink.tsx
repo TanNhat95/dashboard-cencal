fd2a0c8ac48b294a3685ec196e3f39964f76c14d
Tan Nhat <nguyentannhat246@gmail.com>
2025-06-17 00:46:58 +0700

    feat: create base backlink component

---
+++ app/components/BackLink.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const BackLink = () => {
+  return (
+    <div className="p-4 bg-grayScale800 text-white border-b border-gray-700 h-[3.75rem]">
+      <h2 className="text-2xl font-bold">Create Appointment</h2>
+    </div>
+  );
+};
+
+export default BackLink;
