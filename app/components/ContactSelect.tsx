717177e502b13851d6447e7c4de2d2701cafa5a4
Tan Nhat <nguyentannhat246@gmail.com>
2025-06-19 12:27:51 +0700

    feat: add classNames in contact select

--- app/components/ContactSelect.tsx
+++ app/components/ContactSelect.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Controller, FieldValues, Control, Path } from "react-hook-form";
 
+import { twMerge } from "tailwind-merge";
+
 interface CustomContactSelectProps<T extends FieldValues> {
   name: Path<T>;
   control: Control<T>;
   placeholder: string;
   onClick: () => void;
   disabled?: boolean;
+  classNames?: {
+    wrapper?: string;
+    content?: string;
+    iconWrapper?: string;
+    icon?: string;
+  };
 }
 
 const CustomContactSelect = <T extends FieldValues>({
@@ -15,6 +23,7 @@ const CustomContactSelect = <T extends FieldValues>({
   placeholder,
   onClick,
   disabled,
+  classNames = {},
 }: CustomContactSelectProps<T>) => {
   return (
     <Controller
@@ -22,17 +31,29 @@ const CustomContactSelect = <T extends FieldValues>({
       control={control}
       render={({ field }) => (
         <div
-          className="relative w-full text-white px-4 py-3 bg-grayScale600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10 cursor-pointer"
+          className={twMerge(
+            "relative w-full text-white px-4 py-3 bg-grayScale600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10 cursor-pointer",
+            classNames.wrapper
+          )}
           onClick={(e) => {
             e.preventDefault();
             if (!disabled) onClick();
           }}
         >
-          <span className="text-grayScale400">
+          <span className={twMerge("text-grayScale400", classNames.content)}>
             {field.value || placeholder}
           </span>
-          <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-gray-400">
-            <svg className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+          <div
+            className={twMerge(
+              "pointer-events-none absolute inset-y-0 right-3 flex items-center text-gray-400",
+              classNames.iconWrapper
+            )}
+          >
+            <svg
+              className={twMerge("h-4 w-4", classNames.icon)}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
               <path d="M5.23 7.21a.75.75 0 011.06.02L10 10.96l3.71-3.73a.75.75 0 011.06 1.06l-4.24 4.25a.75.75 0 01-1.06 0L5.21 8.27a.75.75 0 01.02-1.06z" />
             </svg>
           </div>
